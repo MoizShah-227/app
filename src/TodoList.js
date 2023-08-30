7f36df5d62ac5136f5ea4d9f71493861d3986848
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2023-08-30 11:58:39 +0500

    Done!

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,46 +1,100 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeTodo, editTodo } from './Todoslice';
-import { getDocs, collection, updateDoc,addDoc,deleteDoc, doc, onSnapshot } from "firebase/firestore"; 
+import { getDocs, collection } from "firebase/firestore"; 
+import { Add_item,FetchData } from './Todoslice';
+
 import db from './Firebase'
+
 const TodoList = ({ handleEditClick, editFormVisibility }) => {
   const todos = useSelector((state) => state.Todo.value);
   const dispatch = useDispatch();
-
   const [editedTodoText, setEditedTodoText] = useState('');
   const [editingTodoId, setEditingTodoId] = useState(null);
   const [todoList, setTodoList] = useState([]);
-  // const todoList = [];
-  
+
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
   const handleEditTodo = (id, editedText) => {
     dispatch(editTodo({ id, updatedTodo: editedText }));
     setEditingTodoId(null);
     setEditedTodoText('');
   };
   
-  const fetchdata = async ()=>{
+  const fetchdata = async () => {
     try {
-      const querySnapshot =  await getDocs(collection(db, 'Todo-List-3'));
-      const fdata =[];
-      querySnapshot.forEach((doc) => {
-        fdata.push({
-          id: doc.id,
-          ...doc.data(),
-        });
-      });
-      setTodoList(fdata)
-     
-     
-    }catch(error){
-      console.log("data not fetch", error)
-    }
+      const querySnapshot = await getDocs(collection(db, 'Todo-List-3'));
+      const fdata = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      
+      setTodoList(fdata);
+      dispatch(Add_item(fdata))
     
+    } catch (error) {
+      console.log("Data not fetched", error);
+    }
   }
-  
-  window.onload =fetchdata()
+  // console.log(todos);
 
-  
-  // return todos?.map((todo) => (
+// console.log("This is",todos)
+  return todoList.map((todo) => (
+    <div key={todo.id} className='todo-box'>
+      <div className='content'>
+        {editingTodoId === todo.id ? (
+          <input
+            type='text'
+            value={editedTodoText}
+            onChange={(e) => setEditedTodoText(e.target.value)}
+          />
+        ) : (
+          <p
+            className='mt-2'
+            style={
+              todo.completed === true
+                ? { textDecoration: 'line-through' }
+                : { textDecoration: 'none' }
+            }
+          >
+            {todo.item}
+          </p>
+        )}
+      </div>
+      <div className='actions-box'>
+        {editFormVisibility === false && (
+          <>
+            {editingTodoId !== todo.id ? (
+              <button
+                onClick={() => setEditingTodoId(todo.id)}
+                className='btn btn-dark m-1'
+              >
+                Edit
+              </button>
+            ) : (
+              <button
+                onClick={() => handleEditTodo(todo.id, editedTodoText)}
+                className='btn btn-dark m-1'
+              >
+                Save
+              </button>
+            )}
+            <button
+              onClick={() => dispatch(removeTodo(todo.id))}
+              className='btn btn-danger m-1'
+            >
+              Delete
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  ));
+
+
+  //  return todos?.map((todo) => (
   //   <div key={todo.id} className='todo-box'>
   //     <div className='content'>
   //       {editingTodoId === todo.id ? (
@@ -87,55 +141,6 @@ const TodoList = ({ handleEditClick, editFormVisibility }) => {
   //     </div>
   //   </div>
   // ));
-
-  return todoList?.map((index,j) => (
-    <div key={todoList.id} className='todo-box'>
-      <div className='content'>
-        {editingTodoId === todoList[j].id ? (
-          <input type='text' value={todoList[j]}/>
-        ) : (
-          <p
-            className='mt-2'
-            style={
-              todoList[j].completed === true
-                ? { textDecoration: 'line-through' }
-                : { textDecoration: 'none' }
-            }
-          >
-            {todoList[j].item}
-          </p>
-        )}
-      </div>
-      <div className='actions-box'>
-        {editFormVisibility === false && (
-          <>
-            {editingTodoId !== todoList[j].id ? (
-              <button
-                onClick={() => setEditingTodoId(todoList.id)}
-                className='btn btn-dark m-1'
-              >
-                Edit
-              </button>
-            ) : (
-              <button
-                onClick={() => handleEditTodo(todoList[j].id, editedTodoText)}
-                className='btn btn-dark m-1'
-              >
-                Save
-              </button>
-            )}
-            <button
-              onClick={() => dispatch(removeTodo(todoList[j].id))}
-              className='btn btn-danger m-1'
-            >
-            Delete
-            </button>
-          </>
-        )}
-      </div>
-    </div>
-  ));
 };
 
 export default TodoList;
-
--- src/Todoslice.js
+++ src/Todoslice.js
@@ -1,8 +1,8 @@
   import { createSlice } from '@reduxjs/toolkit'
   import db from './Firebase'
   // import { dataRef } from './Firebase';
-  import { getDocs, collection, updateDoc,addDoc,deleteDoc, doc, onSnapshot } from "firebase/firestore"; 
-import { act } from 'react-dom/test-utils';
+  import { getDocs, collection, updateDoc,addDoc,deleteDoc, doc, onSnapshot, Firestore } from "firebase/firestore";
+  import { Firebase } from 'firebase/app';
 
   
   const initialState = {
@@ -17,10 +17,11 @@ import { act } from 'react-dom/test-utils';
       reducers: {
         
         Add_item: (state , action) => {
-          state.value.push(action.payload);
+          console.log(action.payload);
+          // state.value.push(action.payload);
           const id =action.payload.id;
           const item =action.payload.todo;
-          console.log(item);
+          // console.log(item);
           try {
             const docRef = addDoc(collection(db, "Todo-List-3"), {
               id: id,
@@ -31,42 +32,14 @@ import { act } from 'react-dom/test-utils';
             console.error("Error adding document: ", e);
           }
         },
-        
-
-        // FetchData: async (state,action) => {
-        //   console.log(action.payload);
-        //   try {
-        //     const querySnapshot =  await getDocs(collection(db, 'Todo-List'));
-        //     const todos = [];
-
-        //     // console.log(querySnapshot)
-        //     querySnapshot.forEach((doc) => {
-        //       todos.push({
-        //         id: doc.id,
-        //         ...doc.data(),
-        //       })
-        //     })
-
-        //     state.value = todos;
-        //     // console.log(state.value)
-        //     // console.log(action.payload)
-
-        //   }catch(error){
-        //     console.log("data not fetch", error)
-        //   }
-        //   },
-
-      
-        
-
-        removeTodo:(state,action)=>{
+  
+        removeTodo: async (state, action) => {
           const todoIdToRemove = action.payload;
           console.log(todoIdToRemove);
-          state.value = state.value.filter(todo => todo.id !== action.payload);
-          
-          try {
-            console.log("DOC ID",getDocs())
-            deleteDoc(doc(db, "Todo-List-3", todoIdToRemove));
+          state.value = state.value.filter(todo => todo.id !== todoIdToRemove);
+          // console.log(doc(todoIdToRemove))
+          try {          
+            await deleteDoc(doc(db, "Todo-List-3", todoIdToRemove));
             console.log("Document deleted with ID: ", todoIdToRemove);
           } catch (e) {
             console.error("Error deleting document: ", e);
@@ -77,11 +50,12 @@ import { act } from 'react-dom/test-utils';
 
         editTodo: (state, action) => {
           const { id, updatedTodo } = action.payload;
-          const todoToEdit = state.value.find(todo => todo.id === id);  
-          console.log(updatedTodo);
+          console.log(id,updatedTodo);
+           const todoToEdit = state.value.find(todo => todo.id === id);  
+          console.log("Hello",todoToEdit);
           try {
             console.log("Edit press");
-            updateDoc(doc(db, "Todo-List-2", id), {
+            updateDoc(doc(db, "Todo-List-3", id), {
               item: updatedTodo,
             });
             console.log("Document updated with ID: ", id);
@@ -89,6 +63,7 @@ import { act } from 'react-dom/test-utils';
             console.error("Error updating document: ", e);
           }
           if (todoToEdit) {
+            console.log("Edit-One",todoToEdit)
             todoToEdit.todo = updatedTodo;
           }
           
